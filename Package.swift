648c9d8fff11e416c55fbf7a2c6a1795e8e4d83a
Fahad <fahad@2doapp.com>
2025-01-23 19:29:34 +0400

    Make mergeable

--- Package.swift
+++ Package.swift
@@ -3,25 +3,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "SmoothGradient",
-    platforms: [
-      .macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
-    ],
-    products: [
-        .library(
-            name: "SmoothGradient",
-            targets: ["SmoothGradient"]),
-        .library(
-            name: "SmoothGradientDynamic",
-            type: .dynamic,
-            targets: ["SmoothGradient"]),
-    ],
-    targets: [
-        .target(
-            name: "SmoothGradient",
-            dependencies: []),
-        .testTarget(
-            name: "SmoothGradientTests",
-            dependencies: ["SmoothGradient"]),
-    ]
+  name: "SmoothGradient",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .watchOS(.v9)
+  ],
+  products: [
+    .library(
+      name: "SmoothGradient",
+      targets: ["SmoothGradient"]),
+    .library(
+      name: "SmoothGradientDynamic",
+      type: .dynamic,
+      targets: ["SmoothGradient"]),
+  ],
+  targets: [
+    .target(
+      name: "SmoothGradient",
+      dependencies: [],
+      linkerSettings: [
+        .unsafeFlags(["-Wl,-make_mergeable"])
+      ]),
+    .testTarget(
+      name: "SmoothGradientTests",
+      dependencies: ["SmoothGradient"]),
+  ]
 )
